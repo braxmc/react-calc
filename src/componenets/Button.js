07252f41b56c54090e113b45bbf5fa10fec83877
Braxton McClellan <thebraxmc@gmail.com>
2021-02-02 22:26:33 -0700

    button.js started

--- src/componenets/Button.js
+++ src/componenets/Button.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {ButtonWrap} from '../styles/ButtonElements'
 
-const Button = () => {
+const Button = (props) => (
+  <ButtonWrap className={`${isOperator(props.children) ? null : 'operator'}`}>
+    {props.children}
+  </ButtonWrap>
+)
 
+const isOperator = val => {
+  return !isNaN(val) || val === '.' || val === '=';
 }
 
 export default Button;
--- src/styles/ButtonElements.js
+++ src/styles/ButtonElements.js
@@ -1,1 +1,19 @@
 import styled from 'styled-component'
+
+export const ButtonWrap = styled.div`
+ height: 4em;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ font-weight: lighter;
+ font-size: 1.4em;
+ background-color: #e0e1e6;
+ color: #888;
+ flex: 1;
+ outline: 1px solid #888;
+`
+
+export const operator = styled.div`
+  background-color: #fe9241;
+  color: #000;
+`
